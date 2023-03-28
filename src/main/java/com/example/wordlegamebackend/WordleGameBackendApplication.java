d025467471dadb22d574e063083674eee195a0d7
Jan Piaskowy <piaskowyjasiek@gmail.com>
2023-03-28 20:34:52 +0200

    init

---
+++ src/main/java/com/example/wordlegamebackend/WordleGameBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.wordlegamebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordleGameBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WordleGameBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/wordlegamebackend/WordleGameBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wordlegamebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordleGameBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
