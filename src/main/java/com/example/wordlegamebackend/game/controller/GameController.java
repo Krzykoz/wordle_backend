e1d1adfc2b7bf88c4df8a959f597b21c103e6ad9
Jan Banka <jan.banka11@gmail.com>
2023-04-24 19:00:01 +0200

    feat: add foundations for game crud

--- src/main/java/com/example/wordlegamebackend/game/controller/GameController.java
+++ src/main/java/com/example/wordlegamebackend/game/controller/GameController.java
@@ -1,4 +1,24 @@
 package com.example.wordlegamebackend.game.controller;
 
+import com.example.wordlegamebackend.game.model.Game;
+import com.example.wordlegamebackend.game.service.GameService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/game")
 public class GameController {
+
+    private final GameService gameService;
+
+    public GameController(GameService gameService) {
+        this.gameService = gameService;
+    }
+    //test
+    @PostMapping
+    public void add(@RequestBody Game game){
+        gameService.addNewGame(game);
+    }
 }
--- src/main/java/com/example/wordlegamebackend/game/repository/GameRepository.java
+++ src/main/java/com/example/wordlegamebackend/game/repository/GameRepository.java
@@ -1,4 +1,10 @@
 package com.example.wordlegamebackend.game.repository;
 
-public class GameRepository {
+import com.example.wordlegamebackend.game.model.Game;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GameRepository extends JpaRepository<Game, Long> {
+
 }
